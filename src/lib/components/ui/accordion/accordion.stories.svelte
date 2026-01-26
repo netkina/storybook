48c45a17a225bc4dabbcd0de38d39b59ee171c92
alekseeva alina <alekskseev@mail.ru>
2026-01-26 12:15:51 +0300

    add accordion

---
+++ src/lib/components/ui/accordion/accordion.stories.svelte
@@ -0,0 +1,26 @@
+<script module lang="ts">
+	import { defineMeta } from "@storybook/addon-svelte-csf";
+	import type { AccordionItems } from "./accordion.svelte";
+    import Accordion from "./accordion.svelte"
+    
+  const { items }: AccordionItems = {
+    items: [
+    { id: "item-1", title: "Title 1", content: "Content 1"},
+    { id: "item-2", title: "Title 2", content: "Content 2"},
+    { id: "item-3", title: "Title 3", content: "Content 3"}
+  ]
+  };
+
+
+    const { Story } = defineMeta({
+        title: "Accordion",
+        component: Accordion,
+        tags: ['autodocs'],
+        args: {
+            items: items 
+        }
+    }) 
+</script>
+
+
+<Story name="Default" args={{ items: items}}/>
---
+++ src/lib/components/ui/accordion/accordion.svelte
@@ -0,0 +1,44 @@
+<script lang="ts" >
+  import { Accordion } from "bits-ui";
+  import CaretDown from "phosphor-svelte/lib/CaretDown";
+  export type AccordionItems = {
+    items: {
+      id: string,
+      title: string,
+      content: string;
+      disabled?: boolean;
+    }[]
+  }
+
+  const props: AccordionItems = $props();
+</script>
+
+<Accordion.Root class="w-full sm:max-w-[70%]" type="multiple">
+  {#each props.items as item (item.id)}
+    <Accordion.Item
+      value={item.id}
+      class="border-dark-10 group border-b px-1.5"
+    >
+      <Accordion.Header>
+        <Accordion.Trigger
+          class="flex w-full flex-1 select-none items-center justify-between py-5 text-[15px] font-medium transition-all [&[data-state=open]>span>svg]:rotate-180"
+        >
+          <span class="w-full text-left">
+            {item.title}
+          </span>
+          <span
+            class="hover:bg-dark-10 inline-flex size-8 items-center justify-center rounded-[7px] bg-transparent"
+          > <CaretDown class="size-[18px] transition-transform duration-200" />
+          </span>
+        </Accordion.Trigger>
+      </Accordion.Header>
+      <Accordion.Content
+        class="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm tracking-[-0.01em]"
+      >
+        <div class="pb-[25px]">
+          {item.content}
+        </div>
+      </Accordion.Content>
+    </Accordion.Item>
+  {/each}
+</Accordion.Root>
